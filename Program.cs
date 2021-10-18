42b013447c4189059084b9310450448868b181e9
Akwasi Appiah <Akwasi.Appiah@capitalontap.com>
2021-10-18 18:13:12 +0100

    minor

--- Program.cs
+++ Program.cs
@@ -8,20 +8,47 @@ namespace PersonDatabase
         static void Main(string[] args)
         {
             Console.WriteLine("WELCOME TO THE PERSON DATABASE");
-            Console.WriteLine("Do you want to enter a User Yes(Y) or No (x)? ");
+            Console.WriteLine("Do you want to enter a User Yes(Y) or No (X)? ");
+
+            /*            string hi = "hi";
+                        ref string initialinput = ref hi;*/
+
             string input = Console.ReadLine();
 
             var people = new List<Person>();
 
-            while(input != "x")
+            /*            switch (initialinput)
+                        {
+                            case "Y":
+                                Person person = IMethods.GetAPersonFromUser();
+                                people.Add(person);
+
+                                Console.WriteLine("Take note of this unique ID number {0}", person.ID);
+                                Console.WriteLine("User successfully added");
+                                Console.Write("Do you want to enter another person? Yes(Y) or No (x)?" );
+                                initialinput = Console.ReadLine();
+                                break;
+                            case "X":
+                                Console.WriteLine("Are you sure?");
+                                initialinput = Console.ReadLine();
+                                break;
+                            default:
+                                Console.Write("Enter a valid key");
+                                initialinput = Console.ReadLine();
+                                break;
+
+                        }
+            */
+            while (input != "x")
             {
                 Person person = IMethods.GetAPersonFromUser();
                 people.Add(person);
 
                 Console.WriteLine("Take note of this unique ID number {0}", person.ID);
                 Console.WriteLine("User successfully added");
-                Console.Write("Do you want to enter another person? Yes(Y) or No (x)?" );
+                Console.Write("Do you want to enter another person? Yes(Y) or No (x)?");
                 input = Console.ReadLine();
+               
             }
 
             Console.WriteLine("Do you need to update the databse? D for delete or F for find or x to exit");
